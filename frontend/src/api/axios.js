9dcbf48c0feb43827efe7fce31c8d131e8110b4a
vikash <vikashtondwals6@gmail.com>
2026-05-05 04:14:43 +0530

    api error

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 });;
-export const getAllUsers = () => API.get("/api/user/all");
-export const deleteUser = (id) => API.delete(`/api/user/${id}`);
+export const getAllUsers = () => API.get("/user/all");
+export const deleteUser = (id) => API.delete(`/user/${id}`);
 export default API;

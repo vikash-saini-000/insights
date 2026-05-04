fe0c5d211a5716f8226e8130fd93636b009ddad3
vikash <vikashtondwals6@gmail.com>
2026-05-05 03:40:02 +0530

    remove errors in controleers

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { storeUser } from "../controllers/userController.js";
+import { storeUser, getAllUsers } from "../controllers/userController.js";
 
 const router = express.Router();
 
